a7230aa7941c2d4f75486d108b9624c159b8ad5c
Kaveesha <kalshandhanasekara@gmail.com>
2025-02-21 00:34:08 +0530

    create search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { Card } from "../components"
+import {useFetch} from "../hooks/useFetch"
 
-export const Search = () => {
+export const Search = ({apiPath}) => {
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get('q');
+  
+  const {data: movies} = useFetch(apiPath, searchQuery);
+
   return (
-    <main>Search</main>
+    <main>
+      <div className='max-w-6xl mx-auto pt-7'>
+        <p className='text-2xl font-semibold text-gray-800 dark:text-gray-200'>
+          {movies.length === 0 ? `No results found for "${searchQuery}"` : `Results for "${searchQuery}"`}
+        </p>
+      </div>
+      <div className="max-w-6xl mx-auto py-7">
+        <div className="flex flex-wrap justify-center">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </div>
+    </main>
   )
 }
