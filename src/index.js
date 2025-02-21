b89acbb12ff14983d373959d21cf8b1d24bc626b
Kaveesha <kalshandhanasekara@gmail.com>
2025-02-21 22:39:24 +0530

    scroll to top

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './tailwind.output.css';
 import App from './App';
+import { ScrollToTop } from './components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>
