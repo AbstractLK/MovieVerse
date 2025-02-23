93dbee2f21a203949b63768bd8a7e2c08850b33a
Kaveesha <kalshandhanasekara@gmail.com>
2025-02-23 13:37:26 +0530

    kk

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,9 +16,9 @@ export const Header = () => {
   }, [darkMode]);
 
   const activeClass =
-    "block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
+    "block py-2 px-3 text-white bg-blue-700 rounded-sm min-md:bg-transparent min-md:text-blue-700 min-md:p-0 min-md:dark:text-blue-500";
   const inactiveClass =
-    "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+    "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 min-md:hover:bg-transparent min-md:hover:text-blue-700 min-md:p-0 min-md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white min-md:dark:hover:bg-transparent dark:border-gray-700";
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -40,7 +40,7 @@ export const Header = () => {
               MovieVerse
             </span>
           </Link>
-          <div className="flex md:order-2">
+          <div className="flex min-md:order-2">
             <button
               onClick={() => setDarkMode(!darkMode)}
               aria-label="Toggle dark mode"
@@ -87,7 +87,7 @@ export const Header = () => {
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
               aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
+              className="min-md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
             >
               <svg
                 className="w-5 h-5"
@@ -106,7 +106,7 @@ export const Header = () => {
               </svg>
               <span className="sr-only">Search</span>
             </button>
-            <div className="relative hidden md:block">
+            <div className="relative hidden min-md:block">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
                   className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -140,7 +140,7 @@ export const Header = () => {
               onClick={() => setHidden(!hidden)}
               data-collapse-toggle="navbar-search"
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg min-md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-search"
               aria-expanded="false"
             >
@@ -165,10 +165,10 @@ export const Header = () => {
           <div
             className={`${
               hidden ? "hidden" : ""
-            } items-center justify-between w-full md:flex md:w-auto md:order-1`}
+            } items-center justify-between w-full min-md:flex min-md:w-auto min-md:order-1`}
             id="navbar-search"
           >
-            <div className="relative mt-3 md:hidden">
+            <div className="relative mt-3 min-md:hidden">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
                   className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -197,7 +197,7 @@ export const Header = () => {
                 />
               </form>
             </div>
-            <ul className="text-center flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="text-center flex flex-col p-4 min-md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 min-md:space-x-8 rtl:space-x-reverse min-md:flex-row min-md:mt-0 min-md:border-0 min-md:bg-white dark:bg-gray-800 min-md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
                   to="/"
